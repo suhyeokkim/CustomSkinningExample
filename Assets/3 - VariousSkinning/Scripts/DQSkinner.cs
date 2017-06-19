0f317e8426987188a67bc8c04c64b6c58a5e2c7c
zizon <tngurdl00@hanmail.net>
2017-06-19 21:31:21 +0900

    Update resources

---
+++ Assets/3 - VariousSkinning/Scripts/DQSkinner.cs
@@ -0,0 +1,51 @@
+﻿namespace Example.VariousSkinning
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public interface IRenderer
+    {
+        void Update();
+        void OnRenderObject();
+    }
+
+    public class DQSkinner : MonoBehaviour
+    {
+        public RenderChunk chunk;
+        public RuntimeRenderChunk chunk2;
+
+        public IRenderer realRenderer;
+
+        void Awake()
+        {
+            
+            if (SystemInfo.supportsComputeShaders)
+            {
+                realRenderer = new ComputeShaderRenderer();
+            }
+        }
+
+        private void OnRenderObject()
+        {
+            realRenderer.OnRenderObject();
+        }
+
+        private void Update()
+        {
+            realRenderer.Update();
+        }
+
+        public class ComputeShaderRenderer : IRenderer
+        {
+            public void OnRenderObject()
+            {
+            }
+
+            public void Update()
+            {
+            }
+        }
+    }
+}
---
+++ Assets/3 - VariousSkinning/Scripts/RenderChunk.cs
@@ -0,0 +1,21 @@
+﻿namespace Example.VariousSkinning
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    [CreateAssetMenu]
+    public class RenderChunk : ScriptableObject
+    {
+        [Header("Input data for skinning")]
+        public Mesh mesh;
+        public Material[] materials;
+    }
+
+    [System.Serializable]
+    public struct RuntimeRenderChunk
+    {
+        public Transform rootBone;
+        public ComputeShader computeShader;
+    }
+}
